d431b129e1b4833393c837a496be2827c9c77427
prasanthgajula08 <prasanth080898@gmail.com>
2021-06-16 18:22:00 -0400

    Add post feature

--- my-app/src/NewsFeed.js
+++ my-app/src/NewsFeed.js
@@ -2,6 +2,11 @@ import React, { useState, useEffect } from 'react';
 
 export default function NewsFeed() {
     return (
-        <h1>NewsFeed</h1>
+        <div>
+            <label for="post">Post something</label>
+            <textarea id="post" name="post"></textarea><br/><br/>
+            <input type="file" id="myFile" name="filename" />
+            <input type="submit" />
+        </div>
     );
 }
