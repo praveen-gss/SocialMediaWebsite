14523921eb7336980019f74aa977764ffb52b992
Praveen Gss <praveengatti123@gmail.com>
2021-06-29 17:01:44 -0400

    Added LoginPage and RegistrationPage

--- my-app/src/Components/LoginPage.js
+++ my-app/src/Components/LoginPage.js
@@ -1,7 +1,51 @@
-import React from 'react';
+import React, { Component } from "react";
+import GoogleLogin from "react-google-login";
 
-export default function LoginPage() {
-    return (
-        <h1>LoginPage</h1>
-    );
+export default class Login extends Component {
+    
+    responseGoogle=(response)=>{
+        console.log(response);
+        
+    }
+    
+    render() {
+        return (
+            <div>
+            <form>
+                <h3>Login</h3>
+
+                <div className="form-group">
+                    <label>Email address</label>
+                    <input type="email" className="form-control" placeholder="Enter email" />
+                </div>
+
+                <div className="form-group">
+                    <label>Password</label>
+                    <input type="password" className="form-control" placeholder="Enter password" />
+                </div>
+
+                <div className="form-group">
+                    <div className="custom-control custom-checkbox">
+                        <input type="checkbox" className="custom-control-input" id="customCheck1" />
+                        <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
+                    </div>
+                </div>
+
+                <button type="submit" className="btn btn-primary btn-block">Submit</button>
+                <p className="forgot-password text-right">
+                    Forgot <a href="#">password?</a>
+                </p>
+                <div>
+                   <GoogleLogin
+                   clientId="791659548746-3fejvutdso1a7cvn681ampphavpseqfg.apps.googleusercontent.com"
+                   buttonText="SignIn with Google"
+                   onSuccess={this.responseGoogle}
+                   onFailure={this.responseGoogle}
+                   cookiePolicy={'single_host_origin'}
+                   />
+                </div>
+            </form>
+            </div>
+        );
+    }
 }
--- my-app/src/Components/RegistrationPage.js
+++ my-app/src/Components/RegistrationPage.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { Component } from "react";
 
-export default function RegistrationPage() {
-    return (
-        <h1>RegistrationPage</h1>
-    );
+export default class RegistrationPage extends Component {
+    render() {
+        return (
+            <form>
+                <h3>signup</h3>
+
+                <div className="form-group">
+                    <label>First name</label>
+                    <input type="text" className="form-control" placeholder="First name" />
+                </div>
+
+                <div className="form-group">
+                    <label>Last name</label>
+                    <input type="text" className="form-control" placeholder="Last name" />
+                </div>
+
+                <div className="form-group">
+                    <label>Email address</label>
+                    <input type="email" className="form-control" placeholder="Enter email" />
+                </div>
+
+                <div className="form-group">
+                    <label>Password</label>
+                    <input type="password" className="form-control" placeholder="Enter password" />
+                </div>
+
+                <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
+                <p className="forgot-password text-right">
+                    Already registered <a href="/LoginPage">sign in?</a>
+                </p>
+            </form>
+        );
+    }
 }
